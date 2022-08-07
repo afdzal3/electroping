9003ff7ce4b732d7bb018f481a12d944ce0c1b87
Afdzal Kamal <afdzal3@gmail.com>
2022-08-08 02:56:48 +0800

    ping app

---
+++ src/main/java/electro/ping/model/Appliance.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package electro.ping.model;
+
+/**
+ *
+ * @author afdzal
+ */
+public class Appliance {
+
+    private String id;
+
+    private String fnr;
+
+    private String status;
+
+    private Long customer_id;
+    
+    public String getId(){
+    return this.id;
+    }
+
+}
--- src/main/java/electro/ping/service/PingService.java
+++ src/main/java/electro/ping/service/PingService.java
@@ -4,9 +4,16 @@
  */
 package electro.ping.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import electro.ping.model.Appliance;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,16 +30,34 @@ public class PingService {
     @Value("${api.path}")
     private String apiPath;
 
+    public String getRandomStatus() {
+        String[] s = {"Connected", "Disconnected"};
+
+        Random ran = new Random();
+        String s_ran = s[ran.nextInt(s.length)];
+
+        return s_ran;
+    }
+
     public void sendPing() {
 
         final String uri = apiPath + "/api/status/list";
 
         String result = "";
         String jsonData = "";
+        Appliance[] apl = null;
 
         try {
-            jsonData = restTemplate.getForObject(uri, String.class);
-            System.out.println(jsonData);
+
+            ResponseEntity<Appliance[]> response
+                    = restTemplate.getForEntity(
+                            uri,
+                            Appliance[].class);
+            apl = response.getBody();
+
+            System.out.println(apl);
+           
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -41,7 +66,18 @@ public class PingService {
 
             while (true) {
                 System.out.println(new Date());
-                Thread.sleep(1 * 1000);
+
+                for (Appliance item : apl) {
+                    String aid = item.getId();
+                    String stat = getRandomStatus();
+                    String uri2 = apiPath + "/api/status/upd/"+aid+"/"+stat;
+                    System.out.println(uri2);
+                    restTemplate.getForEntity(
+                            uri2,Object.class);
+                    
+                }
+
+                Thread.sleep(10 * 1000);
 
             }
         } catch (InterruptedException e) {
